2cd65d105daefa8306ff6cb53ee9552242e0d5db
Dan Nita <danita_ro@yahoo.com>
2022-11-29 17:31:10 +0200

    syntax error corrected

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -32,7 +32,7 @@ module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.22"
-  subnet_ids =      = ["subnet-0dc362f01746e33fc", "subnet-017c136d5f8f51054"] #CHANGE
+  subnet_ids      = ["subnet-0dc362f01746e33fc", "subnet-017c136d5f8f51054"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
